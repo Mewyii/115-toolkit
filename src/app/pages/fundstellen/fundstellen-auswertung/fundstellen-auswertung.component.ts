37fa9474407b524c1ed327d11d7409bef3459d3a
Sebastian Quendt <sebastian.quendt@outlook.de>
2024-07-29 14:08:48 +0200

    angular material update 14-> 15

--- src/app/pages/fundstellen/fundstellen-auswertung/fundstellen-auswertung.component.ts
+++ src/app/pages/fundstellen/fundstellen-auswertung/fundstellen-auswertung.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MatLegacyCheckboxChange as MatCheckboxChange } from '@angular/material/legacy-checkbox';
 import {
   ConverterService,
   FundstellenService,
--- src/app/pages/verteiler/verteiler.component.ts
+++ src/app/pages/verteiler/verteiler.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
 import { ConverterService, SheetDataMapping, XLSService } from 'src/app/services';
 
 export interface KontaktInfo {
