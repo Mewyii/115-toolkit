a57eca1868731198456e80e73dd4a7b9f77cceeb
Sebastian Quendt <sebastian.quendt@outlook.de>
2026-04-01 11:01:19 +0200

    Updated Bot Teilnehmer

--- src/app/pages/chatbot/ki-bot/ki-bots.ts
+++ src/app/pages/chatbot/ki-bot/ki-bots.ts
@@ -2,10 +2,6 @@ import { ChatbotTeilnehmer } from './ki-bot.component';
 
 export const kiBots: ChatbotTeilnehmer[] = [
   {
-    id: '60b5662b-c194-487c-b507-99e724483432',
-    name: 'Frankfurt',
-  },
-  {
     id: 'badc6889-52e0-409e-aa63-f4bb74d809b2',
     name: 'Aachen',
   },
@@ -14,123 +10,139 @@ export const kiBots: ChatbotTeilnehmer[] = [
     name: 'Berlin',
   },
   {
-    id: '25de82c1-8cc3-439c-89cd-42a9424eb274',
-    name: 'Bielefeld',
-  },
-  {
     id: '8d2103bc-98dc-4f6e-87b1-c8d04a6d936e',
     name: 'Essen',
   },
   {
+    id: '60b5662b-c194-487c-b507-99e724483432',
+    name: 'Frankfurt am Main',
+  },
+  {
     id: '940a524c-c020-4d4f-9677-e00f4989fc32',
-    name: 'Grünheide',
-  },
-  {
-    id: '9104b658-d77a-4e91-9f24-db00e412cc43',
-    name: 'Magdeburg',
-  },
-  {
-    id: 'f19a5435-f30b-44da-99d8-79a7e30c773a',
-    name: 'Kassel',
-  },
-  {
-    id: '9d3db172-5128-4cab-9885-39dbe12f182d',
-    name: 'Königstein',
-  },
-  {
-    id: 'cae8bdf4-0e05-44ab-9ec2-fe802204c4ed',
-    name: 'Potsdam',
-  },
-  {
-    id: '890e4618-55cc-4332-9f6d-79af7c3fc43b',
-    name: 'Bernau',
-  },
-  {
-    id: '40d65016-a6de-49cf-a6cf-e4cf8d5d7204',
-    name: 'Schönefeld',
-  },
-  {
-    id: 'e8a8968d-c576-4c7b-b06e-bf60718229e5',
-    name: 'Dahme Spreewald',
+    name: 'Grünheide (Mark)',
   },
   {
     id: 'fb8f45f7-e6af-43f4-9a8e-f6fd761f9f30',
     name: 'Harburg',
   },
   {
-    id: '262a4d6d-dab7-461c-95d5-26d6cf2b07f3',
-    name: 'Karlsruhe',
+    id: '9d3db172-5128-4cab-9885-39dbe12f182d',
+    name: 'Königstein im Taunus',
   },
   {
-    id: '50d66d4e-12f6-4600-a5a3-3bb4a2bd3d92',
-    name: 'Burg (Spreewald)',
-  },
-  {
-    id: '05a341f3-6fe5-4116-8356-b06e6f3dc9c6',
-    name: 'Potsdam-Mittelmark',
-  },
-  {
-    id: 'f5b8df88-b4bd-4883-b079-d95322a3b395',
-    name: 'Erfurt',
-  },
-  {
-    id: '1b1d2681-2a3c-4d5c-a32b-87bcff40cf46',
-    name: 'Oebisfelde-Weferlingen',
-  },
-  {
-    id: '48ea2786-4048-4dce-b66e-7243a3f5d112',
-    name: 'Chemnitz',
-  },
-  {
-    id: 'a7e93901-d8cc-42c9-9e99-bfe90d45b483',
-    name: 'Zoll',
-  },
-  {
-    id: '72b0cd35-6093-479a-b944-72e5c2390e10',
-    name: 'Karlsruhe (LK)',
-  },
-  {
-    id: '3c1fed1d-ec83-4984-8bf3-4f99f0ed5380',
-    name: 'Hamburg',
-  },
-  {
-    id: '7ea93200-477a-4a48-b8b4-a247cce99421',
-    name: 'Stuttgart',
-  },
-  {
-    id: 'bf2e9bfd-d4ad-4928-87ef-c1c1f3b8969b',
-    name: 'Ludwigshafen',
-  },
-  {
-    id: '34ecf714-cb6b-4f54-a61b-720d27823ad2',
-    name: 'Eutin',
-  },
-  {
-    id: '21741939-b3ba-4d07-8969-737395161c7c',
-    name: 'Wedel',
+    id: '9104b658-d77a-4e91-9f24-db00e412cc43',
+    name: 'Magdeburg',
   },
   {
     id: '4981069a-e5b4-46d3-ad0e-05bd66112a11',
     name: 'Bad Oldesloe',
   },
   {
-    id: '1eb39c15-9111-4089-bc15-a192823b9c4a',
-    name: 'Wesel',
+    id: '48ea2786-4048-4dce-b66e-7243a3f5d112',
+    name: 'Chemnitz',
   },
   {
-    id: '125f4c77-b4b3-43f0-957f-e228b91c90e8',
-    name: 'Berlin',
+    id: '3c1fed1d-ec83-4984-8bf3-4f99f0ed5380',
+    name: 'Hamburg',
+  },
+  {
+    id: '262a4d6d-dab7-461c-95d5-26d6cf2b07f3',
+    name: 'Karlsruhe',
+  },
+  {
+    id: 'bf2e9bfd-d4ad-4928-87ef-c1c1f3b8969b',
+    name: 'Ludwigshafen am Rhein',
   },
   {
     id: '1c37cccb-3791-4e2a-b7ab-f2bf167d9c1c',
-    name: 'Main-Taunus',
+    name: 'Main-Taunus-Kreis',
   },
   {
-    id: '87ce9fd2-4220-4f6b-b428-73cac5c2238d',
-    name: 'Prignitz',
+    id: '8b2b8876-aa5e-4461-b967-59ac899b0fa1',
+    name: 'Oldenburg (Oldb)',
+  },
+  {
+    id: 'cae8bdf4-0e05-44ab-9ec2-fe802204c4ed',
+    name: 'Potsdam',
+  },
+  {
+    id: 'c3c9e4e1-f20c-47e1-b82e-411ce70f8bd5',
+    name: 'Trier',
+  },
+  {
+    id: 'd5597a89-6c33-46e6-82cb-479ab09c1c84',
+    name: 'Altlandsberg',
+  },
+  {
+    id: '890e4618-55cc-4332-9f6d-79af7c3fc43b',
+    name: 'Bernau bei Berlin',
+  },
+  {
+    id: '9c54bad7-1e42-4d0b-a765-28a8490282e5',
+    name: 'Brieselang',
+  },
+  {
+    id: '50d66d4e-12f6-4600-a5a3-3bb4a2bd3d92',
+    name: 'Burg (Spreewald)',
+  },
+  {
+    id: 'e8a8968d-c576-4c7b-b06e-bf60718229e5',
+    name: 'Dahme-Spreewald',
   },
   {
     id: '41f90d9d-2e94-4f19-812f-fd0ddb4b0762',
     name: 'Elbe-Elster',
+  },
+  {
+    id: 'f5b8df88-b4bd-4883-b079-d95322a3b395',
+    name: 'Erfurt',
+  },
+  {
+    id: '4cd56fe2-8079-41ee-8b80-c0bd6df81d9f',
+    name: 'Falkensee',
+  },
+  {
+    id: 'ad10dfba-f9ae-4c37-9e09-2f072e909272',
+    name: 'Forst (Lausitz)',
+  },
+  {
+    id: 'c847cfe2-c485-4456-be04-497126f13efa',
+    name: 'Groß Kreutz (Havel)',
+  },
+  {
+    id: '72b0cd35-6093-479a-b944-72e5c2390e10',
+    name: 'Karlsruhe LK',
+  },
+  {
+    id: 'f19a5435-f30b-44da-99d8-79a7e30c773a',
+    name: 'Kassel',
+  },
+  {
+    id: 'be5ea888-0ec6-401f-8559-edca402e5b96',
+    name: 'Ketzin/Havel',
+  },
+  {
+    id: 'f0c1a4b1-ee8d-4825-ad4a-474987cc4bb8',
+    name: 'Kyritz',
+  },
+  {
+    id: '8d3ef2e2-8d38-4dbd-a398-0da26159c622',
+    name: 'Neuruppin',
+  },
+  {
+    id: '40d65016-a6de-49cf-a6cf-e4cf8d5d7204',
+    name: 'Schönefeld',
+  },
+  {
+    id: '7accd3f3-0132-4518-8ff6-07537fd95c40',
+    name: 'Schwielowsee',
+  },
+  {
+    id: '7ea93200-477a-4a48-b8b4-a247cce99421',
+    name: 'Stuttgart',
+  },
+  {
+    id: '21741939-b3ba-4d07-8969-737395161c7c',
+    name: 'Wedel',
   },
 ];
